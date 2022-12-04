3cab8695e8b8b11f21d313af5adfa3e2064e64fa
Saemi H <sstyle940104@gmail.com>
2022-12-04 19:32:59 +0900

    feat: add completed ts study

---
+++ src/epic/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import FirstChapter from "./pages/FirstChapter";
+import FifthChapter from "./pages/FifthChapter";
+import FourthChapter from "./pages/FourthChapter";
+import RestChapter from "./pages/RestChapter";
+import SecondChapter from "./pages/SecondChapter";
+import SixthChapter from "./pages/SixthChapter";
+import ThirdChapter from "./pages/ThirdChapter";
+
+function EpicIndex() {
+  return (
+    <div>
+      <FirstChapter />
+      <SecondChapter />
+      <ThirdChapter />
+      <FourthChapter />
+      <FifthChapter />
+      <SixthChapter />
+      <RestChapter />
+    </div>
+  );
+}
+
+export default EpicIndex;
